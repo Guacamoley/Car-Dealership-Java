6025a292c36c15367aebc982d6c7ea0949fc8b56
Michael Ha <77702012+Guacamoley@users.noreply.github.com>
2022-09-03 22:56:55 -0500

    Create Dealership.java and list attributes and methods from class diagram

---
+++ Program/src/Dealership.java
@@ -0,0 +1,17 @@
+public class Dealership {
+    /*
+    * TODO:
+    * Attributes:
+    *   -dealershipID
+    *   (+)canAcquire - public or private?
+    *   -inventory
+    *
+    * Methods:
+    *   +addVehicle()
+    *   +enableAcquisition()
+    *   +disableAcquisition()
+    *   +listVehicles()
+    *   +toString()
+    *   +getters() & setters()
+    * */
+}
