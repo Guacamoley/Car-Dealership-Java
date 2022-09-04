d9a9f6ee8872109a6f69f81947f33075e57b684e
Michael Ha <77702012+Guacamoley@users.noreply.github.com>
2022-09-03 22:55:36 -0500

    Create Main.java as main runner class

---
+++ Program/src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Evan, Michael, Rodnee, Gabe, Paul
+ * Group 1 Project 1
+ * ICS 372-01
+ * Professor Tim Carlson
+ */
+
+// Main runner class for project
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
