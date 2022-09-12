8a97edbade4bca4087cfcb2d6b1ef50818444cd9
RodneeYang <rodneeyang@gmail.com>
2022-09-12 00:26:16 -0500

    Add GSON library to be used
    Add File class for input.json

--- src/Json.java
+++ src/Json.java
@@ -1,7 +1,20 @@
+;
+
+import com.google.gson.JsonElement;
+
+import java.io.File;
+
 public class Json {
     /*
     * TODO:
     * readFile()
     * exportFile()
     * */
+
+    //File Reader
+    File fileInput = new File("input.json");
+
+
+
+
 }
